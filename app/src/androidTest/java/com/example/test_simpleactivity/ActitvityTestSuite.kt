fa978f3c4947b5f20915e086d25865a02eacb2ca
Ehsan Fallahi <ehsanfallahi@gmail.com>
2020-08-10 10:42:38 +0430

    Add Test Suites

---
+++ app/src/androidTest/java/com/example/test_simpleactivity/ActitvityTestSuite.kt
@@ -0,0 +1,9 @@
+package com.example.test_simpleactivity
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(MainActivityTest::class,
+        SecondaryActivityTest ::class)
+class ActitvityTestSuite
